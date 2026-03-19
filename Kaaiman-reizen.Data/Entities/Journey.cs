8f8fe0c326926f1a8aac9f8223b878c296b815c3
Janno <165158587+BaksteenV20@users.noreply.github.com>
2026-03-19 12:41:43 +0100

    journey entity

---
+++ Kaaiman-reizen.Data/Entities/Journey.cs
@@ -0,0 +1,38 @@
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+
+namespace Kaaiman_reizen.Data.Entities;
+
+public class Journey : IValidatableObject
+{
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Land is verplicht.")]
+    public string Country { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Start datum is verplicht.")]
+    public DateTime StartDate { get; set; }
+
+    [Required(ErrorMessage = "Eind datum is verplicht.")]
+    public DateTime EndDate { get; set; }
+
+    [Required(ErrorMessage = "Aantal busjes is verplicht.")]
+    [Range(0, int.MaxValue, ErrorMessage = "Aantal busjes moet groter of gelijk aan 0 zijn.")]
+    public int? Busses { get; set; }
+
+    [Required(ErrorMessage = "Aantal reizigers is verplicht.")]
+    [Range(1, int.MaxValue, ErrorMessage = "Aantal reizigers moet groter of gelijk aan 1 zijn.")]
+    public int? Travelers { get; set; }
+
+    public List<TravelLeader> TravelLeaders { get; set; } = new();
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (StartDate >= EndDate)
+        {
+            yield return new ValidationResult(
+                "Start datum mag niet later of gelijk zijn aan eind datum",
+                new[] { nameof(StartDate), nameof(EndDate) });
+        }
+    }
+}
