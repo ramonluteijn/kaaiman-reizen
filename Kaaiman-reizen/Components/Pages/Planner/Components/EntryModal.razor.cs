dbe542d3b98a3ebb93b659812f28e5ef3f304957
Swifty <adjagers13@gmail.com>
2026-04-19 15:54:22 +0200

    refactor moved entry modal to it's own component

---
+++ Kaaiman-reizen/Components/Pages/Planner/Components/EntryModal.razor.cs
@@ -0,0 +1,24 @@
+using Kaaiman_reizen.Data.Entities;
+using Microsoft.AspNetCore.Components;
+
+namespace Kaaiman_reizen.Components.Pages.Planner.Components;
+
+public partial class EntryModal : ComponentBase
+{
+    [Parameter] public bool ActionInProgress { get; set; }
+    [Parameter] public List<PlanningVersion> AvailableDrafts { get; set; } = new();
+    
+    // Establishing two-way binding properties for the Draft ID
+    [Parameter] public int? SelectedDraftId { get; set; }
+    [Parameter] public EventCallback<int?> SelectedDraftIdChanged { get; set; }
+    
+    // Callback methods mapped to your buttons
+    [Parameter] public EventCallback OnGenerateNew { get; set; }
+    [Parameter] public EventCallback OnResumeDraft { get; set; }
+
+    protected async Task SelectDraft(int id)
+    {
+        SelectedDraftId = id;
+        await SelectedDraftIdChanged.InvokeAsync(id);
+    }
+}
