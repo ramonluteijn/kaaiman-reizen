4dd9a7393c9e86038a669b16f869de890eeb281d
Jay Dielemans <Jay.dielemans@student.avans.nl>
2026-05-11 14:49:46 +0200

    Commit before merge master in branche

---
+++ Kaaiman-reizen.Data/Services/AccountService.cs
@@ -0,0 +1,29 @@
+﻿using Kaaiman_reizen.Data.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kaaiman_reizen.Data.Services
+{
+    public class AccountService
+    {
+        private readonly MainContext _db;
+
+        public AccountService(MainContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<List<ApplicationUser>> GetAllUsersAsync() => await _db.Users.ToListAsync() ?? new ();
+
+        public List<ApplicationUser> GetMatchingUsers(List<ApplicationUser> users, string? searchTerm)
+        {
+            if (string.IsNullOrWhiteSpace(searchTerm))
+                return [];
+            
+            return users
+                .Where(user =>
+                    (user.UserName != null && user.UserName.Contains(searchTerm, StringComparison.OrdinalIgnoreCase)) ||
+                    (user.Email != null && user.Email.Contains(searchTerm, StringComparison.OrdinalIgnoreCase)))
+                .ToList();
+        }
+    }
+}
