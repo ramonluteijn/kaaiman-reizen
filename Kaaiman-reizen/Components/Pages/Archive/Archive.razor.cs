8ec0ae5b99a77848b9889b53b0e62ce31ab86ced
Janno <165158587+BaksteenV20@users.noreply.github.com>
2026-05-24 14:18:17 +0200

    Added archive page

---
+++ Kaaiman-reizen/Components/Pages/Archive/Archive.razor.cs
@@ -0,0 +1,14 @@
+using Kaaiman_reizen.Data.Services;
+using Kaaiman_reizen.Data.Rules;
+using Kaaiman_reizen.Extensions;
+using Kaaiman_reizen.Helpers;
+using Kaaiman_reizen.Models.ViewModels;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace Kaaiman_reizen.Components.Pages.Archive;
+
+public partial class Archive
+{
+    
+}
